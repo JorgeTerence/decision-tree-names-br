66aeef2b559908f68148f43ad85f0e496f4dc4dd
JorgeTerence <jorge.terence@outlook.com>
2025-07-14 10:28:15 -0300

    feat: version 2.0 start

--- README.md
+++ README.md
@@ -3,12 +3,34 @@
 ## TODO
 
 - [ ] port to C
-- [ ] port to Java
 - [ ] port to JavaScript
 - [ ] make an interactive website and collect feedback
 - [ ] make a LinkedIn post about this
-- [ ] expand the training set
-- [ ] add more features to the model
+- [x] expand the training set
+- [x] add more features to the model
+
+## Version 2.0
+
+**Training set length:** 807 rows
+
+### Model features
+
+1. ends_in_vowel
+2. nasal_count
+3. sharp_count
+4. {_vowel_}\_count
+5. {_pronounciation_}\_r_count
+6. ends_in_vowel
+7. ending*vowel*{_vowel_}
+8. ending*consonant*{_h, l, m, m, n, r, s, z or null_}
+9. starts_in_vowel
+10. starting*vowel*{_vowel_}
+11. starting*consonant*{_consonant_}
+12. s_count
+
+### Preview
+
+![decision graph version 2](./.github/assets/tree_v2.png)
 
 ## Version 1.0
 
@@ -20,12 +42,12 @@
 2. ends_in_vowel
 3. nasal_count
 4. sharp_count
-5. {_vowel_}_count
-6. {_pronounciation_}_r_count
-7. ending_vowel_{_vowel_}
-8. ending_consonant_{_h, l, m, m, n, r, s, z or null_}
+5. {_vowel_}\_count
+6. {_pronounciation_}\_r_count
+7. ending*vowel*{_vowel_}
+8. ending*consonant*{_h, l, m, m, n, r, s, z or null_}
 
-### Result
+### Preview
 
 ![decision graph version 1](./.github/assets/tree_v1.png)
 
--- src/features.py
+++ src/features.py
@@ -15,68 +15,88 @@ from typing import Any, Hashable, Optional, List
 
 @dataclass
 class Featured:
-    original: str
+    # original: str
     syllables: List[str]
     normalized_syllables: List[str]
+
     ends_in_vowel: bool
     ending_vowel: Optional[str]
     ending_consonant: Optional[str]
+
+    starts_in_vowel: bool
+    starting_vowel: str
+    starting_consonant: Optional[str]
+
     nasal_count: int
     sharp_count: int
+
     a_count: int
     i_count: int
     u_count: int
     e_count: int
     o_count: int
+
     rome_r_count: int
     potato_r_count: int
     laughed_r_count: int
+
+    s_count: int
+
     gender: str
 
     @staticmethod
-    def from_row(row: dict[Hashable,Any]) -> "Featured":
-        syllables = split_syllables(row["name"])
+    def from_row(row: dict[Hashable, Any]) -> "Featured":
+        _name = asciify(row["name"])
+        syllables = split_syllables(_name)
 
-        word = asciify(row["name"])
-        normalized_syllables = split_syllables(word)
-
-        ends_in_vowel = is_vowel(word[-1])
+        ends_in_vowel = is_vowel(_name[-1])
 
         if ends_in_vowel:
-            ending_vowel = word[-1]
+            ending_vowel = _name[-1]
             ending_consonant = None
         else:
-            ending_consonant = word[-1]
+            ending_consonant = _name[-1]
             ending_vowel = next((c for c in syllables[-1][::-1] if is_vowel(c)), None)
 
-        nasal_count = count_nasal_phonemes(word)
-        sharp_count = count_sharp_phonemes(word)
+        starts_in_vowel = is_vowel(_name[0])
 
-        a_count = word.count("a")
-        i_count = word.count("i")
-        u_count = word.count("u")
-        e_count = word.count("e")
-        o_count = word.count("o")
+        if starts_in_vowel:
+            starting_vowel = _name[0]
+            starting_consonant = None
+        else:
+            starting_consonant = _name[0]
+            try:
+                starting_vowel = [char for char in syllables[0] if is_vowel(char)][0]
+            except Exception as e:
+                print(syllables)
+                raise e
 
-        rome_r, potato_r, laughed_r = count_r_pronounciations(word)
+        rome_r, potato_r, laughed_r = count_r_pronounciations(_name)
 
         return Featured(
-            original=word,
+            # original=row["name"],
             syllables=syllables,
-            normalized_syllables=normalized_syllables,
+            normalized_syllables=split_syllables(_name),
             ends_in_vowel=ends_in_vowel,
             ending_vowel=ending_vowel,
             ending_consonant=ending_consonant,
-            nasal_count=nasal_count,
-            sharp_count=sharp_count,
-            a_count=a_count,
-            i_count=i_count,
-            u_count=u_count,
-            e_count=e_count,
-            o_count=o_count,
+            nasal_count=count_nasal_phonemes(_name),
+            sharp_count=count_sharp_phonemes(_name),
+            a_count=_name.count("a"),
+            i_count=_name.count("i"),
+            u_count=_name.count("u"),
+            e_count=_name.count("e"),
+            o_count=_name.count("o"),
             rome_r_count=rome_r,
             potato_r_count=potato_r,
             laughed_r_count=laughed_r,
+            starts_in_vowel=starts_in_vowel,
+            starting_vowel=starting_vowel,
+            starting_consonant=starting_consonant,
+            s_count=_name.count("ss")
+            + _name.count("ce")
+            + _name.count("ci")
+            + row["name"].count("ç")
+            + _name.startswith("s"),
             gender=row["gender"],
         )
-
